cdf950ebb4b3773a32c930d6dac6faa25d18600a
super_fkmt <kd1362203@st.kobedenshi.ac.jp>
2025-01-08 13:53:12 +0900

    update:モジュール更新時にモジュールの名前を取得する方法を変更

--- commands/update.py
+++ commands/update.py
@@ -4,6 +4,7 @@
 
 import subprocess
 import os
+import fnmatch
 
 def Register(subparsers):
     parser = subparsers.add_parser("update", help="モジュールを更新する")
@@ -11,4 +12,8 @@ def Register(subparsers):
 def Execute(args):
     nowDir=os.path.basename(os.getcwd())
     print(nowDir)
-    subprocess.run(["cmake",f"-DPROJ_NAME={nowDir}","-DPROJ_TYPE=STATIC"])
+
+    # mainディレクトリの中のインクルードファイルからプロジェクト名を取得
+    files = os.listdir(os.getcwd()+"/main/include")
+    projName = [i for i in files if i.endswith(".hpp") == True]
+    subprocess.run(["cmake",f"-DPROJ_NAME={os.path.splitext(projName[0])[0]}","-DPROJ_TYPE=STATIC"])
