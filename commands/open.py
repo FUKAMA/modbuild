8d7a6c5a8d2f497137245ccf41a992e4b93dc950
super_fkmt <kd1362203@st.kobedenshi.ac.jp>
2025-01-11 20:46:57 +0900

    update:openでソリューションを起動するように変更

--- commands/open.py
+++ commands/open.py
@@ -6,20 +6,33 @@ import os
 import subprocess
 from tkinter import filedialog
 from tkinter import Tk, filedialog
+import json
+
 
 def Register(subparsers):
     parser = subparsers.add_parser("open", help="モジュールの開発環境を開く")
 
 def Execute(args):
 
-    # ソリューションのパスを指定
-    solution_path = filedialog.askopenfilename(
-        title="プロジェクトファイルを選択",
-        filetypes=[("ソリューションファイル", "*.sln")]
-    )
-    print(solution_path)
-    dir_name=os.path.dirname(solution_path)
-    print(dir_name)
+    # proj_data_path = "projData.json"
+    # json.load(proj_data_path)
+
+    # open()   
+    with open("projData.json", "r", encoding="utf-8") as file:
+        data = json.load(file)  # JSONをPythonの辞書型に変換
+    projName=data.get("ProjName")
+
+    os.system(f"start {projName}.sln")
+
+
+    # # ソリューションのパスを指定
+    # solution_path = filedialog.askopenfilename(
+    #     title="プロジェクトファイルを選択",
+    #     filetypes=[("ソリューションファイル", "*.sln")]
+    # )
+    # print(solution_path)
+    # dir_name=os.path.dirname(solution_path)
+    # print(dir_name)
 
     # os.startfile(solution_path)
 
